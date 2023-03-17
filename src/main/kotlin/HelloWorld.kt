788c5688db3c074d141f89a649eb5b295061f792
ifmanullang <ifmanullang@gmail.com>
2023-03-18 01:04:40 +0700

    kotlin dasar

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,5 @@
+fun main (){
+    println("Hello World")
+    print("My name Irfan Fernandez Manullang and i will be a good programer in the world")
+    print(", Aminnn")
+}
---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,34 @@
+fun main() {
+    //Integer Number
+    var umur : Byte = 55;
+    var nik : Long = 12072302039870003;
+
+    //output Integer Number
+    println("umur saya 30 tahun lagi adalah : " + umur);
+    println("nik : " + nik);
+
+    println("===========================================================================================")
+
+
+    //Floating Point Number
+    var nilai : Float = 80.5F;
+    var waktu : Double = 07.55;
+
+    //output Floatimg Point Number
+    println("Nilai : " + nilai);
+    println("sekarang oukul :" + waktu);
+
+    println("===========================================================================================")
+
+
+    //Literals
+    var binar : Int = 0b0111000110;
+    var campur : Int = 0xAB19CD;
+
+    //output Literals
+    println(binar);
+    println(campur);
+
+    println("===========================================================================================")
+
+}
